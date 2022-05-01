5debb679fb2e2afdf6770ff982715bd6d0e70e87
althafvly <althafvly@gmail.com>
2022-05-01 17:12:18 +0530

    update patch script

--- patch.sh
+++ patch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-. build/envsetup.sh
-
 PATCH_DIR=${PWD}
-
 echo "TOP: $PATCH_DIR"
+
+if [ -f "build/envsetup.sh" ]; then
+    . build/envsetup.sh
+else
+    echo "Can't find build/envsetup.sh."
+    exit
+fi
 
 # ----------------------------------
 # Colors
@@ -26,42 +30,73 @@ LIGHTPURPLE='\033[1;35m'
 LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 
+function download_patch {
+    echo -e "${GREEN}Downloading patch $2...${NOCOLOR}"
+    cd $PATCH_DIR
+    wget -O $2 https://raw.githubusercontent.com/sdm870/android_manifest/lineage-19.1/patches/$1/$2
+    echo -e "${GREEN}.................${NOCOLOR}"
+}
+
 function apply_patch {
     echo -e "${GREEN}Applying patch...${NOCOLOR}"
     echo -e "${LIGHTBLUE}Target repo:${NOCOLOR} $1"
     echo -e "${LIGHTBLUE}Patch file:${NOCOLOR} $2"
-    echo ""
 
-    cd $1
-    git am -3 --ignore-whitespace $2
-    cd $PATCH_DIR
-    echo ""
+    if [ -d "$1" ]; then
+        #cd $1
+        if [ -f "$2" ]; then
+            git am -3 --ignore-whitespace $PATCH_DIR/$2
+            rm $PATCH_DIR/$2
+        else
+            echo "Can't find patch file $2, skipping patch"
+        fi
+        cd $PATCH_DIR && echo ""
+    else
+        echo "Can't find dir $1, skipping patch"
+    fi
+    echo -e "${GREEN}.................${NOCOLOR}"
 }
 
 #################################################################
-# CHERRYPICKS                                                   #
+# GERRIT CHERRYPICKS                                            #
 #                                                               #
 # Examples:                                                     #
 # repopick [CHANGE_NUMBER]                                      #
 #################################################################
 
-# android_device_xiaomi_sm8250-common
+if ! command -v repopick &>/dev/null; then
+    echo "repopick could not be found"
+    exit
+else
+    # android_device_xiaomi_sm8250-common
+    #repopick
 
-# android_device_xiaomi_alioth
+    # android_device_xiaomi_alioth
+    #repopick
 
-# android_frameworks_base
-repopick 320714 329326
+    # android_frameworks_base
+    repopick 320714 329326
 
-# android_packages_apps_LineageParts
-repopick 326739
+    # android_packages_apps_LineageParts
+    repopick 326739
 
-# android_packages_apps_Updater
-repopick 323341
+    # android_packages_apps_Updater
+    repopick 323341
 
-# android_vendor_lineage
+    # android_vendor_lineage
+    #repopick
+fi
 
 #################################################################
-# PATCHES
+# Download patches
+#
+# Example: download_patch [FILE_DIR_IN_PATCHES] [PATCH_FILE]
+#################################################################
+download_patch android_frameworks_base 0001-base-Spoof-build-fp-for-GMS.patch
+download_patch android_frameworks_base 0002-SpoofFingerprint-spoof-photos.patch
+
+#################################################################
+# Apply patches
 #
 # Example: apply_patch [REPO_DIR] [PATCH_FILE]
 #################################################################

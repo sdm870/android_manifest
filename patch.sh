88f991b2edb1976b78361630d73a1f3974847766
Mohammed Althaf <althafvly@gmail.com>
2022-05-18 14:53:19 +0530

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -97,9 +97,9 @@ if ! command -v repopick &>/dev/null; then
     exit
 else
     # android_device_xiaomi_sm8250-common
-    repopick 318741 331246 331247
+    repopick 318741 331246 331247 331504
     repopick 331131 330511 331272 331273
-    repopick 331404 331409 331410 331411 
+    repopick 331404 331409 331410 
 
     # android_device_xiaomi_alioth
     repopick 331274
@@ -125,7 +125,6 @@ else
 
     # android_vendor_lineage
     #repopick -f 330449
-    repopick 331362
 
     echo "Nothing to repopick here"
 fi
